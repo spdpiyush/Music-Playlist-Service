b4761c3ec8b7250655909b3d303b7f501d2d6f0e
pikumar <pikumar@quotient.com>
2021-11-02 12:02:10 +0530

    Setting up the Project Structure

---
+++ src/main/java/com/piyush/musicplaylistservice/MusicPlaylistServiceApplication.java
@@ -0,0 +1,13 @@
+package com.piyush.musicplaylistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlaylistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPlaylistServiceApplication.class, args);
+	}
+
+}
